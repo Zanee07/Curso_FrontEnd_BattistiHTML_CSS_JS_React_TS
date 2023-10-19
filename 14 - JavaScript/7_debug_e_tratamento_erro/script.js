20be72e32089105817dcc66cebff823e37443c1b
ZaNe <134183116+Zanee07@users.noreply.github.com>
2023-10-19 20:57:55 -0300

    Update script.js

--- 14 - JavaScript/7_debug_e_tratamento_erro/script.js
+++ 14 - JavaScript/7_debug_e_tratamento_erro/script.js
@@ -1,49 +1,83 @@
-// 1 - strict
-"use strict"; // Usados em projetos profissionais, ele identifica o erro no inspecionar do google
+// // 1 - strict
+// "use strict"; // Usados em projetos profissionais, ele identifica o erro no inspecionar do google
 
-//opa = "teste";
+// //opa = "teste";
 
-const opa = "teste"
+// const opa = "teste"
 
-// 2 - Método de debug: console log
-let a = 1
-let b = 2
+// // 2 - Método de debug: console log
+// let a = 1
+// let b = 2
 
-if(a == 1) {
-    a = b + 2;
-}
+// if(a == 1) {
+//     a = b + 2;
+// }
 
-console.log(a)
+// console.log(a)
 
-for(let i = 0; i < b; i++) {
-    a = a + 2;
-    console.log(a)
-}
+// for(let i = 0; i < b; i++) {
+//     a = a + 2;
+//     console.log(a)
+// }
 
-if(a > 5) {
-    a = 25;
-}
+// if(a > 5) {
+//     a = 25;
+// }
 
-console.log(a)
+// console.log(a)
 
-// 3 - debugger
-let c = 1
-let d = 2
+// // 3 - debugger
+// let c = 1
+// let d = 2
 
-if(c ==1) {
-    c = d + 2
-}
+// if(c ==1) {
+//     c = d + 2
+// }
 
-// debugger; //no debbug do navegador ele trava aqui
+// // debugger; //no debbug do navegador ele trava aqui
 
-for(let i = 0; i < d; i++) {
-    c = c + 2;
-}
+// for(let i = 0; i < d; i++) {
+//     c = c + 2;
+// }
 
-console.log("executou o loop");
+// console.log("executou o loop");
 
-debugger;
+// //debugger;
 
-if (c > 5) {
-    c = 25;
-}
+// if (c > 5) {
+//     c = 25;
+// }
+
+// // 4 - tratamento de dados
+// function checkNumber(n) {
+//     const result = Number(n)
+
+//     if(Number.isNaN(result)) {
+//         console.log("Valor incorreto");
+//         return;
+//     }
+
+//     console.log("Valor correto!");
+//     return result;
+// }
+
+// checkNumber(5);
+// checkNumber("10");
+// checkNumber({});
+// checkNumber("teste");
+
+// // 5 - exceptions
+// let x = 10
+
+// if(x != 11) {
+//     throw new Error("O valor de x não pode ser diferente de 11")
+// }
+
+
+// 6 try catch
+try {
+    const soma = x + y
+} catch(error) {
+    console.log(`Erro no programa: ${error}`);
+} 
+
