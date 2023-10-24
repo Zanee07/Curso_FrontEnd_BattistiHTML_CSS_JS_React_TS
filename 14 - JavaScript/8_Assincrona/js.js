34334b1a58f7328268ca4b4af670b7179d9340ef
ZaNe <134183116+Zanee07@users.noreply.github.com>
2023-10-23 22:20:00 -0300

    Update js.js

--- 14 - JavaScript/8_Assincrona/js.js
+++ 14 - JavaScript/8_Assincrona/js.js
@@ -1,1 +1,53 @@
-// Assincrona - repostas que não são obtidads de formas imediatas
+// // Assincrona - repostas que não são obtidads de formas imediatas
+
+
+// // 1 - setTimeout
+// console.log("Ainda não executou")
+
+// setTimeout(function() {
+
+//     console.log("Requisição assíncrona")
+
+// }, 2000)
+
+// console.log("Ainda não executou 2")
+
+// // 2 setInterval
+// console.log("Ainda não começou")
+
+// setInterval(function() {
+//     console.log("Intervalo assíncrono");
+// }, 3000);
+
+// console.log("Ainda não começou");
+
+// 3 - Promise (Prometamos que vai ter um valorr esse valor pode chegar em um valor futuro)
+
+const promessa = Promise.resolve(5 + 5);
+
+console.log("Algum código");
+
+promessa
+    .then((value) => {
+        console.log(`A soma é ${value}`);
+        return value;
+    })
+    .then((value) => value - 1)
+    .then((value) => console.log(`Agora é ${value}`));
+
+console.log("Outro código");
+
+// 4 - Falha na promise
+Promise.resolve(4 * "asd")
+.then((n) => {
+    if(Number.isNaN(n)) {
+        throw new Error("Valores inválidos")
+    }
+})
+.catch((err) => console.log(`Um erro ocorreu: ${err}`))
+
+// 5 - rejeição 
+function checkNumber(n) {
+    return new Promise.
+}
+
